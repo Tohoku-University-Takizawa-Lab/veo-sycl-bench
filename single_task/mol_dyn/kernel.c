904ac54156af51c9c438f0065699cabe85f8a6ae
lijiahao <lijh.sdu@gmail.com>
2021-12-21 18:46:18 +0900

    mol_dyn

---
+++ single_task/mol_dyn/kernel.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    float x, y, z;
+} Atom;
+
+void P3dyn(Atom* input, Atom* output, int* neighbour, int *size) {
+    // printf("mol_dyn *size=%d\n", *size);
+    int neighCount = 15;
+    int cutsq = 50;
+    int lj1 = 20;
+    float lj2 = 0.003f;
+    int inum = 0;
+    for (int i = 0; i < *size; ++i) {
+        Atom ipos = input[i];
+        Atom f = {0.0f, 0.0f, 0.0f};
+        int j = 0;
+        while (j < neighCount) {
+            int jidx = neighbour[j * inum + i];
+            Atom jpos = input[jidx];
+            // Calculate distance
+            float delx = ipos.x - jpos.x;
+            float dely = ipos.y - jpos.y;
+            float delz = ipos.z - jpos.z;
+            float r2inv = delx * delx + dely * dely + delz * delz;
+            // If distance is less than cutoff, calculate force
+            if (r2inv < cutsq) {
+                r2inv = 10.0f / r2inv;
+                float r6inv = r2inv * r2inv * r2inv;
+                float forceC = r2inv * r6inv * (lj1 * r6inv - lj2);
+                f.x += delx * forceC;
+                f.y += dely * forceC;
+                f.z += delz * forceC;
+            }
+            j++;
+        }
+        output[i] = f;
+    }
+}
---
+++ single_task/mol_dyn/mol_dyn.cpp
@@ -0,0 +1,81 @@
+#include <CL/sycl.hpp>
+#include <chrono>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+REGISTER_KERNEL(dyn);
+using namespace cl::sycl;
+
+typedef struct {
+    float x, y, z;
+} Atom;
+
+int main(int argc, char** argv) {
+    auto start = std::chrono::steady_clock::now();
+    int size = 5;
+    if (argc > 1)
+        size = std::stoi(argv[1]);
+
+    accelerator_selector as;
+    queue q(as);
+    // queue q;
+
+    std::vector<Atom> input(size);
+    std::vector<Atom> output(size);
+    std::vector<int> neighbour(size);
+    for (int i = 0; i < size; i++) {
+        Atom temp = {(float)i, (float)i, (float)i};
+        input[i] = temp;
+    }
+    for (int i = 0; i < size; i++) {
+        neighbour[i] = i + 1;
+    }
+
+    buffer<Atom> in_buff(input.data(), range<1>(size));
+    buffer<int> neb_buff(neighbour.data(), range<1>(size));
+    buffer<Atom> out_buff(output.data(), range<1>(size));
+    buffer<int> n_buff(&size, range<1>(1));
+
+    q.submit([&](handler& cgh) {
+        auto in_access = in_buff.get_access<access::mode::read_write>(cgh);
+        auto out_access = out_buff.get_access<access::mode::read_write>(cgh);
+        auto neb_access = neb_buff.get_access<access::mode::read_write>(cgh);
+        auto no_access = n_buff.get_access<access::mode::read_write>(cgh);
+
+        cgh.single_task<class dyn>([=]() {
+            int neighCount = 15;
+            int cutsq = 50;
+            int lj1 = 20;
+            float lj2 = 0.003f;
+            int inum = 0;
+            for (int i = 0; i < no_access[0]; ++i) {
+                Atom ipos = in_access[i];
+                Atom f = {0.0f, 0.0f, 0.0f};
+                int j = 0;
+                while (j < neighCount) {
+                    int jidx = neb_access[j * inum + i];
+                    Atom jpos = in_access[jidx];
+                    float delx = ipos.x - jpos.x;
+                    float dely = ipos.y - jpos.y;
+                    float delz = ipos.z - jpos.z;
+                    float r2inv = delx * delx + dely * dely + delz * delz;
+                    if (r2inv < cutsq) {
+                        r2inv = 10.0f / r2inv;
+                        float r6inv = r2inv * r2inv * r2inv;
+                        float forceC = r2inv * r6inv * (lj1 * r6inv - lj2);
+                        f.x += delx * forceC;
+                        f.y += dely * forceC;
+                        f.z += delz * forceC;
+                    }
+                    j++;
+                }
+                out_access[i] = f;
+            }
+        });
+    });
+    q.wait();
+    auto end = std::chrono::steady_clock::now();
+    std::cout << "mol_dyn size=" << size << " runtime(" << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << ")" << std::endl;
+    return 0;
+}
